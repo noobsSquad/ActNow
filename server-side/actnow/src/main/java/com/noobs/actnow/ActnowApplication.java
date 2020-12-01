e92c9dd276b74c7d3f891c9caaba1a7a09a9710d
Tomas Chavez <tomasc691@gmail.com>
2020-12-01 16:31:55 -0600

    initial bootstrap

---
+++ server-side/actnow/src/main/java/com/noobs/actnow/ActnowApplication.java
@@ -0,0 +1,13 @@
+package com.noobs.actnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActnowApplication.class, args);
+	}
+
+}
---
+++ server-side/actnow/src/test/java/com/noobs/actnow/ActnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobs.actnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
